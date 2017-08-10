fa665a8c7b4d92f1d9717a70605ac97a881e0a8b
魏喜胜 <956656628@qq.com>
2017-08-10 14:41:11 +0800

    更新文件

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
-'use strict';
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-function timeout(ms) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(resolve, ms, 'done');
-    });
-}
-
-timeout(1000).then(function (value) {
-    console.log("%c" + value, 'background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f00),' + 'color-stop(0.15, #FF7D00), color-stop(0.3, #ff0), color-stop(0.45, #0f0), color-stop(0.6, #00f),' + 'color-stop(0.75, #0ff), color-stop(0.9, #f0f), color-stop(1, #000) );color:transparent;' + '-webkit-background-clip: text;font-size:20px;');
-});
-
-var result = Array.from(new Set([1, 3, 5, 8, 4, 1, 5, 9, 6, 4, 2, 4]));
-
-var re = [].concat(_toConsumableArray(new Set([1, 3, 5, 8, 4, 1, 5, 9, 6, 4, 2, 4])));
+"use strict";function ಠ_ಠ750(o){if(Array.isArray(o)){for(var r=0,t=Array(o.length);r<o.length;r++)t[r]=o[r];return t}return Array.from(o)}function ಠ_ಠ751(o){return new Promise(function(r,t){setTimeout(r,o,"done")})}ಠ_ಠ751(1e3).then(function(o){console.log("%c"+o,"background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f00),color-stop(0.15, #FF7D00), color-stop(0.3, #ff0), color-stop(0.45, #0f0), color-stop(0.6, #00f),color-stop(0.75, #0ff), color-stop(0.9, #f0f), color-stop(1, #000) );color:transparent;-webkit-background-clip: text;font-size:20px;")});var result=Array.from(new Set([1,3,5,8,4,1,5,9,6,4,2,4])),re=[].concat(ಠ_ಠ750(new Set([1,3,5,8,4,1,5,9,6,4,2,4])));
